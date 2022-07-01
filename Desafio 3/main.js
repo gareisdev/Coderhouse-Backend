d1df573eba1b1ae46c6581c25944438373a34095
Leonel Gareis <leonelgareis@Leonels-MacBook-Air.local>
2022-06-30 21:15:10 -0300

    Desafio 3: Express

---
+++ Desafio 3/main.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { Contenedor } = require("./container/container");
+
+// App
+const app = express();
+
+// Endpoints
+app.get("/productos", (req, res) => {
+    const contenedor = new Contenedor("products");
+
+    let data = contenedor.getAll();
+
+    if (!data) {
+        res.status(200).send({ message: "No hay productos" });
+    } else {
+        res.status(200).send(data);
+    }
+});
+
+app.get("/productoRandom", (req, res) => {
+    const contenedor = new Contenedor("products");
+
+    const data = contenedor.getAll();
+    const randomProduct = data[Math.floor( Math.random() * data.length )];
+    res.status(200).send( { product : randomProduct} )
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto 3000");
+})
